1c7331a28cd7c84b8673943051423079ef8d798a
gabriel <gm.silva111297@gmail.com>
2018-04-06 21:04:25 -0300

    primeiro codigo

---
+++ teste.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+        
+        int a,b,d;
+        
+        a = 6;
+        b = 0;
+        d = a/b;
+        
+        System.out.println("resultado: "+d);
+    }
+}
